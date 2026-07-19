26627414294982e33f31159c113c7ac87493f139
nour bouslimi <bouslimi.nourelhouda@esprit.tn>
2026-07-19 13:30:52 +0100

    test output 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://stage4eme-backend.onrender.com',
-  socketUrl: 'https://stage4eme-backend.onrender.com'
+  socketUrl: 'https://stage4eme-backend.onrender.com',
+  mapTileUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 };
