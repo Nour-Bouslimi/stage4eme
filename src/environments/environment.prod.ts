bf04eb3900ba681dd2f4445303ce8d782989fde3
nour bouslimi <bouslimi.nourelhouda@esprit.tn>
2026-07-19 12:05:44 +0100

    config production Vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.deliverease.com',
-  socketUrl: 'https://api.deliverease.com',
-  mapTileUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+  apiUrl: 'https://stage4eme-backend.onrender.com',
+  socketUrl: 'https://stage4eme-backend.onrender.com'
 };
